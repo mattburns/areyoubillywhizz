32a67f72464213c63f1268a446d02e05bac70e13
floater81 <floater81@df1711a4-3e26-0410-b9ee-8f899ccb018a>
2007-05-20 15:08:57 +0000

    Added 3x3 scramblers/Scramble3by3.java

---
+++ areyoubillywhizz/src/scramblers/Scramble3by3.java
@@ -0,0 +1,70 @@
+/**
+ * Copyright 2007 Matt Burns
+ * 
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ * 
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ * 
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package scramblers;
+
+import sun.reflect.ReflectionFactory.GetReflectionFactoryAction;
+
+/**
+ * @author Matt Burns
+ *
+ */
+public class Scramble3by3 implements ScrambleGenerator {
+
+    private enum FACE {U, D, L, R, F, B};
+    private int scrambleLength = 25;
+    
+    /* (non-Javadoc)
+     * @see scramblers.ScrambleGenerator#getName()
+     */
+    public String getName() {
+        return "3x3 Cube";
+    }
+
+    /* (non-Javadoc)
+     * @see scramblers.ScrambleGenerator#getScramble()
+     */
+    public String getScramble() {
+        String scramble = "";
+        FACE lastFace = getRandomFace();
+        for (int i = 0 ; i < scrambleLength ; i++) {
+            FACE nextFace = getRandomFace();
+            while (nextFace.toString().equals(lastFace.toString())) {
+                nextFace = getRandomFace();
+            }
+            lastFace = nextFace;
+            scramble += nextFace.toString();
+            if (getRandomBoolean()) {
+                scramble += "2";
+            } else {
+                if (getRandomBoolean()) {
+                    scramble += "'";
+                }
+            }
+            scramble += " ";
+        }
+        return scramble.trim();
+    }
+
+    private boolean getRandomBoolean() {
+        return (Math.random() >= 0.5);
+    }
+    
+    private FACE getRandomFace() {
+        int index = (int)Math.floor(Math.random() * FACE.values().length);
+        return FACE.values()[index];
+    }
+
+}
---
+++ areyoubillywhizz/test/scramblers/Scramble3by3Test.java
@@ -0,0 +1,43 @@
+package scramblers;
+
+import junit.framework.TestCase;
+
+public class Scramble3by3Test extends TestCase {
+    
+    Scramble3by3 threeByThree;
+
+    public void setUp() {
+        threeByThree = new Scramble3by3();
+    }
+    
+    /*
+     * Test method for 'scramblers.Scramble3by3.getName()'
+     */
+    public void testNameIsCorrect() {
+        assertEquals("3x3 Cube", threeByThree.getName());
+    }
+
+    /*
+     * Test method for 'scramblers.Scramble3by3.getScramble()'
+     */
+    public void testScrambleIsRightLength() {
+        String scramble = threeByThree.getScramble();
+        int moves = scramble.split(" ").length;
+        System.out.println(scramble);
+        assertEquals(scramble + " should have had 25 moves", 25, moves);
+    }
+    
+    /*
+     * Test method for 'scramblers.Scramble3by3.getScramble()'
+     */
+    public void testScrambleHasNoMovesOfSameFaceAdjacent() {
+        String scramble = threeByThree.getScramble();
+        String moves[] = scramble.split(" ");
+        for (int i = 0; i < moves.length - 1; i++) {
+            if (moves[i].charAt(0) == moves[i + 1].charAt(0)) {
+                fail("adjacent face turning at index " + i + ": " + scramble);
+            }
+        }
+    }
+
+}
